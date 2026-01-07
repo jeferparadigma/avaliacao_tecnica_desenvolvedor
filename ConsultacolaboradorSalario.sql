4d66d48c3bb0d219ffb6502f3e7ddf44a8ff2275
Jefer Goncalves <jeferparadigma@gmail.com>
2026-01-06 21:17:03 -0300

    CTE  – ColaboradoresRanqueados Cria uma tabela temporária em memória com os dados já preparados. Faz o JOIN entre Pessoa e Departamento.

--- ConsultacolaboradorSalario.sql
+++ ConsultacolaboradorSalario.sql
@@ -1,4 +1,8 @@
-SELECT 
+WITH ColaboradoresRanqueados AS (
+    /* LÓGICA DE PREPARAÇÃO: 
+       Aqui  SQL resolve a partição e o ranking em memória.
+    */
+    SELECT 
         d.Nome AS Departamento,
         p.Nome AS Pessoa,
         p.Salario,
@@ -6,3 +10,17 @@ SELECT
             PARTITION BY p.DeptId 
             ORDER BY p.Salario DESC
         ) AS TopSalario
+    FROM dbo.Pessoa AS p WITH (NOLOCK) -- Dica de performance para leitura
+    INNER JOIN dbo.Departamento AS d ON p.DeptId = d.Id
+)
+/* LÓGICA DE NEGÓCIO: 
+   Apenas filtra o resultado desejado.
+*/
+SELECT 
+    Departamento, 
+    Pessoa, 
+    Salario
+FROM 
+    ColaboradoresRanqueados
+WHERE 
+    TopSalario = 1;
