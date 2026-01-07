bb844a71167df81260edd643717c14b531b66d92
Jefer Goncalves <jeferparadigma@gmail.com>
2026-01-06 22:47:03 -0300

    Main para exucução com array

--- ProfissionalTree/Program.cs
+++ ProfissionalTree/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ProfissionalTree;
+
+class Program
+{
+    static void Main()
+    {
+        ITreeBuilder builder = new BinaryTreeBuilder();
+
+        int[] scenario1 = { 3, 2, 1, 6, 0, 5 };
+        var tree1 = builder.Build(scenario1);
+        Console.WriteLine("Cenário 1:");
+        TreePrinter.Print(tree1);
+
+        Console.WriteLine();
+
+        int[] scenario2 = { 7, 5, 13, 9, 1, 6, 4 };
+        var tree2 = builder.Build(scenario2);
+        Console.WriteLine("Cenário 2:");
+        TreePrinter.Print(tree2);
+    }
+}
